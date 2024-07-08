9f238d2315f323df13d9e3a1e24b4828ec022185
aacs <antonio.costa.salgado@gmail.com>
2024-07-08 19:38:17 +0100

    Rework on pv volumes

--- start-cluster.sh
+++ start-cluster.sh
@@ -9,18 +9,20 @@ mkdir -p ${SHARED_VOLUME}
 if [ -f /etc/machine-id ]; then
   cp -f /etc/machine-id ${SHARED_VOLUME}/
 else
-  #touch ${SHARED_VOLUME}/machine-id
-  $(uuidgen | sed 's/-//g') > ${SHARED_VOLUME}/machine-id
+  uuidgen -r -x | tr -d '-' > ${SHARED_VOLUME}/machine-id
 fi
 
+    # --registry-create k3d-registry.localhost:5000 \
 k3d cluster create $CLUSTER_NAME \
     --api-port 6443 \
+    --registry-create k3d-registry.localhost:5000 \
     --port "9090:9090@loadbalancer" \
     --port "4222:4222@loadbalancer" \
     --port "3000:3000@loadbalancer" \
     --port "3100:3100@loadbalancer" \
     --port "4318:4320@loadbalancer" \
     --port "4566:4566@loadbalancer" \
+    --port "5432:5432@loadbalancer" \
     --servers 1 \
     --agents 3 \
     --k3s-arg '--disable=metrics-server@server:*' \
